fbb9e1d8868aabfbba0eeac3294ddb0657381b6e
Davi-Arauj <moreiradaavi@gmail.com>
2021-08-03 20:15:09 -0300

    Iniciando Projeto!

---
+++ src/main/java/digital/innovation/one/personapi/PersonapiApplication.java
@@ -0,0 +1,13 @@
+package digital.innovation.one.personapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonapiApplication.class, args);
+	}
+
+}

5b46e8f7fc228e432919f497c429574ad09ff4d8
Davi-Arauj <moreiradaavi@gmail.com>
2021-08-10 19:49:16 -0300

    Created DTO

---
+++ src/main/java/digital/innovation/one/personapi/dto/request/PersonDTO.java
@@ -0,0 +1,36 @@
+package digital.innovation.one.personapi.dto.request;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonDTO {
+
+    private Long id;
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String firstName;
+
+    @NotEmpty
+    @Size(min = 2, max = 100)
+    private String lastName;
+
+    @NotEmpty
+    @CPF
+    private String cpf;
+
+    private String birthDate;
+
+    @Valid
+    @NotEmpty
+    private List<PhoneDTO> phones;
+}

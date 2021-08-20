14af07336482488026b316db848e0ef262d8b871
Davi-Arauj <moreiradaavi@gmail.com>
2021-08-20 19:37:27 -0300

    Mapstruct okgit add ., adicionando o swagger

---
+++ target/generated-sources/annotations/digital/innovation/one/personapi/mapper/PersonMapperImpl.java
@@ -0,0 +1,117 @@
+package digital.innovation.one.personapi.mapper;
+
+import digital.innovation.one.personapi.dto.request.PersonDTO;
+import digital.innovation.one.personapi.dto.request.PersonDTO.PersonDTOBuilder;
+import digital.innovation.one.personapi.dto.request.PhoneDTO;
+import digital.innovation.one.personapi.dto.request.PhoneDTO.PhoneDTOBuilder;
+import digital.innovation.one.personapi.entity.Person;
+import digital.innovation.one.personapi.entity.Person.PersonBuilder;
+import digital.innovation.one.personapi.entity.Phone;
+import digital.innovation.one.personapi.entity.Phone.PhoneBuilder;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2021-08-20T19:28:46-0300",
+    comments = "version: 1.3.1.Final, compiler: Eclipse JDT (IDE) 1.3.1300.v20210419-1022, environment: Java 16.0.1 (Oracle Corporation)"
+)
+public class PersonMapperImpl implements PersonMapper {
+
+    @Override
+    public Person toModel(PersonDTO personDTO) {
+        if ( personDTO == null ) {
+            return null;
+        }
+
+        PersonBuilder person = Person.builder();
+
+        if ( personDTO.getBirthDate() != null ) {
+            person.birthDate( LocalDate.parse( personDTO.getBirthDate(), DateTimeFormatter.ofPattern( "dd-MM-yyyy" ) ) );
+        }
+        person.cpf( personDTO.getCpf() );
+        person.firstName( personDTO.getFirstName() );
+        person.id( personDTO.getId() );
+        person.lastName( personDTO.getLastName() );
+        person.phones( phoneDTOListToPhoneList( personDTO.getPhones() ) );
+
+        return person.build();
+    }
+
+    @Override
+    public PersonDTO toDTO(Person person) {
+        if ( person == null ) {
+            return null;
+        }
+
+        PersonDTOBuilder personDTO = PersonDTO.builder();
+
+        if ( person.getBirthDate() != null ) {
+            personDTO.birthDate( DateTimeFormatter.ISO_LOCAL_DATE.format( person.getBirthDate() ) );
+        }
+        personDTO.cpf( person.getCpf() );
+        personDTO.firstName( person.getFirstName() );
+        personDTO.id( person.getId() );
+        personDTO.lastName( person.getLastName() );
+        personDTO.phones( phoneListToPhoneDTOList( person.getPhones() ) );
+
+        return personDTO.build();
+    }
+
+    protected Phone phoneDTOToPhone(PhoneDTO phoneDTO) {
+        if ( phoneDTO == null ) {
+            return null;
+        }
+
+        PhoneBuilder phone = Phone.builder();
+
+        phone.id( phoneDTO.getId() );
+        phone.number( phoneDTO.getNumber() );
+        phone.type( phoneDTO.getType() );
+
+        return phone.build();
+    }
+
+    protected List<Phone> phoneDTOListToPhoneList(List<PhoneDTO> list) {
+        if ( list == null ) {
+            return null;
+        }
+
+        List<Phone> list1 = new ArrayList<Phone>( list.size() );
+        for ( PhoneDTO phoneDTO : list ) {
+            list1.add( phoneDTOToPhone( phoneDTO ) );
+        }
+
+        return list1;
+    }
+
+    protected PhoneDTO phoneToPhoneDTO(Phone phone) {
+        if ( phone == null ) {
+            return null;
+        }
+
+        PhoneDTOBuilder phoneDTO = PhoneDTO.builder();
+
+        phoneDTO.id( phone.getId() );
+        phoneDTO.number( phone.getNumber() );
+        phoneDTO.type( phone.getType() );
+
+        return phoneDTO.build();
+    }
+
+    protected List<PhoneDTO> phoneListToPhoneDTOList(List<Phone> list) {
+        if ( list == null ) {
+            return null;
+        }
+
+        List<PhoneDTO> list1 = new ArrayList<PhoneDTO>( list.size() );
+        for ( Phone phone : list ) {
+            list1.add( phoneToPhoneDTO( phone ) );
+        }
+
+        return list1;
+    }
+}
